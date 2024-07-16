1d84793beaf2e232ea245803a7d87ac0f764e8d4
mahgah189 <mkbk189@gmail.com>
2024-07-15 21:52:56 -0400

    set up firebase, configuring functions and server for oauth

---
+++ functions/index.js
@@ -0,0 +1,72 @@
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const cors = require("cors")({origin: true});
+
+const tokenEndpoint = "https://accounts.spotify.com/api/token";
+
+const generateRandomString = length => {
+  let randomStr;
+  const possibleChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabsdefghijklmnopqrstuvwxyz1234567890";
+  for (let i = 0; i < length; i++) {
+    randomStr += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+  };
+  return randomStr;
+};
+
+const getSpotifyAccessToken = async (clientId, clientSecret) => {
+  const headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+  };
+  // eslint-disable max-len
+  const body = `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`;
+  // eslint-disable max-len
+  try {
+    const response = await fetch(tokenEndpoint, {
+      method: "POST",
+      headers: headers,
+      body: body,
+      redirect: "follow",
+    });
+    return await response.json();
+  } catch (error) {
+    logger.error("Error fetching Spotify access token:", error);
+    throw new Error("Unable to fetch Spotify access token");
+  }
+};
+
+exports.getSpotifyToken = onRequest(
+  {secrets: ["SPOTIFY_CLIENT_SECRET"]},
+  (request, response) => {
+    cors(request, response, async () => {
+      try {
+        const token = await getSpotifyAccessToken(
+          process.env.SPOTIFY_CLIENT_ID,
+          process.env.SPOTIFY_CLIENT_SECRET,
+        );
+        response.send({
+          "status": "success",
+          "data": token,
+        });
+      } catch (error) {
+        response.status(500).send("Error getting Spotify access token");
+      }
+    });
+  },
+);
+
+exports.oauthLogin = onRequest(
+  (request, response) => {
+    const origin = req.headers.origin;
+    const redirect_uri = `${origin}/callback`;
+    const state = generateRandomString(16);
+    const scope = "streaming user-read-email user-read-private";
+    const allowedOrigins = [
+      "http://localhost:5173",
+      "spotify-audio-visualizer.web.app",
+    ];
+    if(!allowedOrigins.includes(origin)) {
+      return res.status(403).json({ error: 'Forbidden origin' });
+    };
+    
+  },
+);
---
+++ src/components/oauth/LoginButton/LoginButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function LoginButton() {
+  return (
+    <></>
+  )
+};
+
+export default LoginButton;
